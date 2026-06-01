0e02d9f9396fe4a51bfafd3b136d2c2e63e9e4a7
Daniel Diaz <danozaid556@gmail.com>
2026-05-31 23:01:20 -0600

    test extremo 2

--- web/src/pages/AgregarLocal.jsx
+++ web/src/pages/AgregarLocal.jsx
@@ -175,7 +175,9 @@ function AgregarLocal() {
   return (
     <DashboardLayout>
       <div className="min-h-screen bg-zinc-950 text-white px-5 md:px-8 py-10">
-        <h1 className="text-4xl font-black text-red-500">PRUEBA NUEVA INTERFAZ</h1>
+        <h1 style={{color:"yellow",fontSize:"80px"}}>
+AAAAAAAAAAAAA
+</h1>
         <p className="text-zinc-400 mt-3 max-w-3xl">
           Registra tu local. Se guardará como pendiente y será visible en la plataforma cuando tenga suscripción activa.
         </p>
